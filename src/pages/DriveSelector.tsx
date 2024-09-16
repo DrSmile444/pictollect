2a3da4ae7cd346b9cad860b1a65a93b02245dbb9
Dmytro Vakulenko <dvakulenko@liveperson.com>
2024-09-16 16:15:35 +0300

    feat: add skeleton to drive selector

--- src/pages/DriveSelector.tsx
+++ src/pages/DriveSelector.tsx
@@ -1,5 +1,5 @@
 // DriveSelector.tsx
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, Skeleton, Stack, Typography } from '@mui/material';
 import { Drive } from 'move-from-sd/src/interfaces';
 import React, { FC, useEffect, useState } from 'react';
 
@@ -9,14 +9,21 @@ import { useOs } from '../hooks';
 
 export const DriveSelector: FC = () => {
   const [drives, setDrives] = useState<Drive[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const { setDrive, drive, setDirectory, setStep } = usePhotoContext();
 
   const { fetchDrives } = useOs();
 
   useEffect(() => {
+    setIsLoading(true);
     // Fetch drives when the component mounts
-    fetchDrives().then(setDrives).catch(console.error);
+    fetchDrives()
+      .then(setDrives)
+      .catch(console.error)
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   const handleSelectChange = (drive: Drive) => {
@@ -33,7 +40,13 @@ export const DriveSelector: FC = () => {
       <Typography variant="h2" typography="h4">
         Select a Drive
       </Typography>
-      <DriveSelectorList onSelect={handleSelectChange} drives={drives} drive={drive} />
+      {isLoading && (
+        <Stack gap={1} direction="column" alignItems="center" width="100%">
+          <Skeleton variant="rounded" width="100%" height={88} />
+          <Skeleton variant="rounded" width="100%" height={88} />
+        </Stack>
+      )}
+      {!isLoading && <DriveSelectorList onSelect={handleSelectChange} drives={drives} drive={drive} />}
       <Stack gap={1} direction="row" alignItems="right">
         <Button variant="contained" color="primary" disabled={!drive} onClick={handleNextClick}>
           Next
