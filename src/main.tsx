eac5886c30335e20ef2b85adc38e9599ed903602
Dmytro Vakulenko <dvakulenko@liveperson.com>
2024-09-19 12:57:46 +0300

    feat: add resources

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { PhotoProvider } from './context';
 
-const root = createRoot(document.body);
+const root = createRoot(document.querySelector('#root'));
 root.render(
   <PhotoProvider>
     <App />
